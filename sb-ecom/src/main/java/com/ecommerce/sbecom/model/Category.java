68003feb55f2c010fac5a9868e11a66baab71a4b
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-08-23 01:00:11 +0530

    "Managing Id's of category service"

---
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/model/Category.java
@@ -0,0 +1,53 @@
+package com.ecommerce.sbecom.model;
+
+import java.util.Objects;
+
+public class Category {
+    private Long categoryId;
+    private String categoryName;
+
+    public Category() {
+    }
+
+    public Category(Long categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryId=" + categoryId +
+                ", categoryName='" + categoryName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(categoryId, category.categoryId) && Objects.equals(categoryName, category.categoryName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(categoryId, categoryName);
+    }
+}

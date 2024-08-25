e5311de4b99b82e3891d97ddebca7c775b58afa5
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-08-25 23:06:31 +0530

    "Implemented APIResponse"

--- sb-ecom/src/main/java/com/ecommerce/sbecom/exception/MyGlobalExceptionHandler.java
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/exception/MyGlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.ecommerce.sbecom.exception;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.ecommerce.sbecom.payload.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -25,15 +26,15 @@ public class MyGlobalExceptionHandler {
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException e) {
-        String message = e.getMessage();
-        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    public ResponseEntity<APIResponse> myResourceNotFoundException(ResourceNotFoundException e) {
+        APIResponse apiResponse = new APIResponse(e.getMessage(), false);
+        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ApiException.class)
-    public ResponseEntity<String> myApiException(ApiException e) {
-        String message = e.getMessage();
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<APIResponse> myApiException(ApiException e) {
+        APIResponse apiResponse = new APIResponse(e.getMessage(), false);
+        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
     }
 
 }

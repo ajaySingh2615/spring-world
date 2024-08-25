9612e8bfc355d76e2b0af106dbe9b37a18af92b2
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-08-25 23:01:47 +0530

    "ASC and DSC in pagination"

--- sb-ecom/src/main/java/com/ecommerce/sbecom/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.sbecom.controller;
 
+import com.ecommerce.sbecom.config.AppConstants;
 import com.ecommerce.sbecom.payload.CategoryDTO;
 import com.ecommerce.sbecom.payload.CategoryResponse;
 import com.ecommerce.sbecom.service.CategoryService;
@@ -22,10 +23,12 @@ public class CategoryController {
 
     @GetMapping("/public/categories")
     public ResponseEntity<CategoryResponse> getAllCategories(
-            @RequestParam(name = "pageNumber") Integer pageNumber,
-            @RequestParam(name = "pageSize") Integer pageSize
+            @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_CATEGORIES_BY, required = false) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIRECTION_BY, required = false) String sortOrder
     ) {
-        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize);
+        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize, sortBy, sortOrder);
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
